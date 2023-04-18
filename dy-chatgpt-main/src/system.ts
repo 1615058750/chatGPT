db3a6887dfa50a848a17b02df82243e51937f74d
1615058750 <130210985+1615058750@users.noreply.github.com>
2023-04-18 21:45:30 +0800

    Add files via upload

---
+++ dy-chatgpt-main/src/system.ts
@@ -0,0 +1,28 @@
+import type { Model } from "./types"
+
+export const defaultSetting = {
+  continuousDialogue: true,
+  archiveSession: false,
+  openaiAPIKey: "",
+  openaiAPITemperature: 60,
+  password: "",
+  systemRule: "",
+  model: "gpt-3.5-turbo" as Model
+}
+
+export const defaultMessage = ` OpenAI UIUI
+- 如果本项目对你有所帮助可以跟我们联系。
+- 本网站仅作为项目演示,如有需求，可定制小程序，APP或H5。
+- 联系电话: 15985578888`
+
+export type Setting = typeof defaultSetting
+
+export const defaultResetContinuousDialogue = false
+
+export const defaultMaxInputTokens: Record<Model, number> = {
+  "gpt-3.5-turbo": 3072,
+  "gpt-4": 6144,
+  "gpt-4-32k": 24576
+}
+
+export const defaultModel: Model = "gpt-3.5-turbo"
